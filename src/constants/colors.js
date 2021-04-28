ae443a167a64cc83599f248b90919d71b12eab14
Magdalena Price <maprice@mit.edu>
2021-04-27 20:28:10 -0400

    Adding some default colors

---
+++ src/constants/colors.js
@@ -0,0 +1,19 @@
+const colors = {
+    black:"#090A0C",
+    white:"#FFFFFF",
+    dark_grey:"#3D4251", 
+    medium_grey:"#585F74",
+    light_grey:"#969DB0",
+    soft_green:"#98C3A5",
+    bold_green:"#65A478",
+    soft_yellow:"#FEC171",
+    bold_yellow:"#FEA634",
+    soft_pink:"#F6B7BC",
+    bold_pink:"#EF8089",
+    soft_blue:"#7AA0CD",
+    bold_blue:"#4E7FBC",
+    soft_purple:"#C383BF",
+    bold_purple:"#7C3C78",
+}
+
+export default colors;
