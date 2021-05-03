34400ed26b9b6d216d55fa383cc319bd16f429b8
Magdalena Price <maprice@mit.edu>
2021-05-02 20:35:08 -0400

    minor update to intro

--- src/Introduction/Intro.js
+++ src/Introduction/Intro.js
@@ -8,7 +8,7 @@ import colors from './../constants/colors';
 import states from './../constants/states';
 
 
-const summary = "With the continually increasing popularity of \"fast-fashion\" companies, it is becoming more and more important to understand the social and ethical context in which these companies operate, and the impacts they can have on the planet, animals, and people. After a curated narrative experience, we invite you to explore the policies several clothing companies have with regards to animal welfare, environmental responsibility, and workers' rights, with data gathered from ";
+const summary = "With the continually increasing popularity of \"fast-fashion\" companies, it is becoming more and more important to understand the social and ethical context in which such companies operate, as well as the influence they have on the planet, animals, and people. After a curated narrative experience, we invite you to explore the policies several clothing companies have with respect to animal welfare, environmental responsibility, and workers' rights, within a visualization containing data gathered from ";
 const goodOnYouURL = "https://goodonyou.eco/";
 
 
