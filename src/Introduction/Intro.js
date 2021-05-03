1c48f7da734690a89810df15d6974003be503f30
Magdalena Price <maprice@mit.edu>
2021-05-02 20:24:33 -0400

    Updated Introduction

--- src/Introduction/Intro.js
+++ src/Introduction/Intro.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react';
 
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import { IconButton } from '@material-ui/core';
+
 import Button from '@material-ui/core/Button';
 import colors from './../constants/colors';
 import states from './../constants/states';
+
+
+const summary = "With the continually increasing popularity of \"fast-fashion\" companies, it is becoming more and more important to understand the social and ethical context in which these companies operate, and the impacts they can have on the planet, animals, and people. After a curated narrative experience, we invite you to explore the policies several clothing companies have with regards to animal welfare, environmental responsibility, and workers' rights, with data gathered from ";
+const goodOnYouURL = "https://goodonyou.eco/";
 
 
 class Intro extends Component {
@@ -21,17 +28,44 @@ class Intro extends Component {
     render() {
         return (
         <div>
-            <h2 style={{color: colors.black}}>Welcome to the Introduction</h2>
-            <Button variant="contained" onClick={this.onNarrativeSubmit} style={{
-            color: colors.medium_grey,
-            size: "small",
-            backgroundColor: colors.soft_green,
-            }}>Head to Narrative</Button>
-            <Button variant="contained" onClick={this.onVisualizationSubmit} style={{
-            color: colors.medium_grey,
-            size: "small",
-            backgroundColor: colors.soft_blue,
-            }}>Head to Visualization</Button>
+            <h1 style={{fontSize:'48px', margin: ['20px','0px','20px','0px'], textAlign: 'center', color: colors.black}}>Passion for Fashion</h1>
+            <div style={{ marginLeft:'20px', marginRight:'20px', textAlign: 'center'}}>
+                <i style={{fontSize:'24px', color: colors.black}}>
+                    {summary}
+                    <a href={goodOnYouURL} style={{color:colors.bold_blue}} rel="noreferrer">good on you</a>
+                    <a>.</a>
+                </i>
+            </div>
+            <div style={{ marginLeft:'40px'}} >
+                <h2>Narrative Experiences</h2>
+                <div style={{display: 'flex'}}>
+                <div style={{fontSize:'24px', marginTop: '8px', color: colors.soft_green}}>Everlane</div>
+                <IconButton
+                children={<ArrowForwardIosIcon/>}
+                color="primary"
+                variant="contained" 
+                onClick={this.onNarrativeSubmit} 
+                style={{
+                    color: colors.soft_green,
+                    size: "small",
+                    backgroundColor: "transparent",}}
+                />
+                </div>
+                <div style={{display: 'flex'}}>
+                    <div style={{fontSize:'24px', marginTop: '8px', color: colors.soft_blue}}>No Thanks, Take Me to the Main Visualization</div>
+                    <IconButton
+                    children={<ArrowForwardIosIcon/>}
+                    color="primary"
+                    variant="contained" 
+                    onClick={this.onVisualizationSubmit} 
+                    style={{
+                        color: colors.soft_blue,
+                        size: "small",
+                        backgroundColor: "transparent",}}
+                    />
+                </div>
+            </div>
+            
         </div>
         );
     }
