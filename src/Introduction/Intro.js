ea9474627926b60dfaa9e4f081e3a404d8cd2000
Magdalena Price <maprice@mit.edu>
2021-05-19 00:45:00 -0400

    Intro page formatting, image buttons

--- src/Introduction/Intro.js
+++ src/Introduction/Intro.js
@@ -10,9 +10,16 @@ import narratives from './../constants/narratives';
 
 import './../App.css';
 
+// images
+import everlane from './Images/everlane_logo.png';
+import nike from './Images/nike_logo2.png';
+import hm from './Images/hm_logo.png';
+
 const summary = "With the continually increasing popularity of \"fast-fashion\" companies, it is becoming more and more important to understand the social and ethical context in which such companies operate, as well as the influence they have on the planet, animals, and people. After a curated narrative experience, we invite you to explore the policies several clothing companies have with respect to animal welfare, environmental responsibility, and workers' rights, within a visualization containing data gathered from ";
 const goodOnYouURL = "https://goodonyou.eco/";
+const githubURL = "https://github.com/6859-sp21/final-project-passion-for-fashion";
 
+const narrative_summary = "Click on a company's logo to learn more about them via a step-through narrative experience. If you want to skip ahead to the main visualization, click the \"No Thanks, Let's Explore\" button down below."
 
 class Intro extends Component {
     constructor(props) {
@@ -47,40 +54,75 @@ class Intro extends Component {
         );
     } 
 
+    buttonChildrenImage = (image, imageSize) => {
+        return (
+            <div style={{}}>
+                <img style={{height: imageSize}} src ={image}/>
+                {/* <div style={{fontSize: textSize}}>{text}</div> */}
+            </div>
+        );
+    }
+
     render() {
         return (
         <div>
-            <h1 style={{fontSize:'48px', margin: ['20px','0px','20px','0px'], textAlign: 'center', color: colors.black}}>Passion for Fashion</h1>
-            <div style={{ marginLeft:'20px', marginRight:'20px', textAlign: 'center'}}>
-                <i style={{fontSize:'20px', color: colors.medium_grey}}>
-                    {summary}
-                    <a href={goodOnYouURL} target="_blank" style={{color:colors.bold_blue}} rel="noreferrer">good on you</a>
-                    <a>.</a>
-                </i>
-            </div>
-            <div style={{ marginLeft:'40px'}} >
-                <h2>Narrative Experiences</h2>
-                <div style={{display: 'flex'}}>
-                    <IconButton 
-                        className="click" 
-                        style={{fontSize:"20px", color:colors.soft_green, backgroundColor: "transparent"}} 
-                        onClick={this.onEverlaneSubmit}>
-                        {this.buttonChildrenLTR("Everlane")}
-                    </IconButton>
-                    <IconButton 
-                        className="click" 
-                        style={{fontSize:"20px", color:colors.soft_pink, backgroundColor: "transparent"}} 
-                        onClick={this.onNikeSubmit}>
-                        {this.buttonChildrenLTR("Nike")}
-                    </IconButton>
-                    <IconButton 
-                        className="click" 
-                        style={{fontSize:"20px", color:colors.soft_yellow, backgroundColor: "transparent"}} 
-                        onClick={this.onHMSubmit}>
-                        {this.buttonChildrenLTR("H&M")}
-                    </IconButton>
+            <div style={{marginLeft:'10vw', width: '80vw', height: '80vh'}}>
+                <h1 style={{fontSize:'48px', margin: ['20px','0px','20px','0px'], textAlign: 'center', color: colors.black}}>Passion for Fashion</h1>
+                <div style={{ marginLeft:'20px', marginRight:'20px', display:'flex', justifyContent:'center'}}>
+                    <i style={{fontSize:'20px', color: colors.medium_grey}}>
+                        {summary}
+                        <a href={goodOnYouURL} target="_blank" style={{color:colors.bold_blue}} rel="noreferrer">good on you</a>
+                        <a>.</a>
+                    </i>
                 </div>
-                <div style={{display: 'flex'}}>
+                <div style={{ marginLeft:'40px'}} >
+                    <div style={{display: 'flex', justifyContent:'center'}}>
+                        <h1>Narrative Experiences</h1>
+                    </div>
+                    <div style={{display: 'flex', justifyContent:'center'}}>
+                        <i style={{fontSize:'20px', color: colors.medium_grey}}>
+                            {narrative_summary}
+                        </i>
+                    </div>
+                    <div style={{display: 'flex', justifyContent: 'space-evenly', marginTop:'20px'}}>
+                        <IconButton 
+                            className="click" 
+                            style={{color:colors.soft_pink, backgroundColor: "transparent"}} 
+                            onClick={this.onNikeSubmit}>
+                            {this.buttonChildrenImage(nike, "60px")}
+                        </IconButton>
+                        <IconButton 
+                            className="click" 
+                            style={{color:colors.soft_green, backgroundColor: "transparent"}} 
+                            onClick={this.onEverlaneSubmit}>
+                            {this.buttonChildrenImage(everlane, "90px")}
+                        </IconButton>
+                        <IconButton 
+                            className="click" 
+                            style={{color:colors.soft_yellow, backgroundColor: "transparent"}} 
+                            onClick={this.onHMSubmit}>
+                            {this.buttonChildrenImage(hm,  "90px",)}
+                        </IconButton>
+                    </div>
+                    
+                </div>
+            </div>
+            <div style={{marginLeft:'10vw', width: '80vw', height: '20vh'}}>
+                <div style={{display: 'flex', justifyContent:'space-between'}}>
+                    <div>
+                        <div style={{fontStyle:'italic', fontFamily:'sans-serif', color:colors.light_grey}}>
+                        Melody Phu
+                        </div>
+                        <div style={{fontStyle:'italic', fontFamily:'sans-serif', color:colors.light_grey}}>
+                        Magdalena Price
+                        </div>
+                        <div style={{fontStyle:'italic', fontFamily:'sans-serif', color:colors.light_grey}}>
+                        Spring 2021 6.859 Final Project
+                        </div>
+                        <a href = {githubURL} target="_blank" style={{color:colors.soft_blue, fontStyle:'italic', fontFamily:'sans-serif'}} rel="noreferrer">
+                            Project Github
+                        </a>
+                    </div>
                     <IconButton 
                         className="click" 
                         style={{fontSize:"20px", color:colors.soft_blue, backgroundColor: "transparent"}} 
@@ -89,7 +131,6 @@ class Intro extends Component {
                     </IconButton>
                 </div>
             </div>
-            
         </div>
         );
     }
